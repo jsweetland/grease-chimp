b95a7c7c6270afe0c2e31197b5ab787fefc7552a
Jamie Sweetland <slimindie@gmail.com>
2023-10-19 11:10:51 -0400

    refactor: use function components instead of class components for pages

--- ui/src/pages/home.js
+++ ui/src/pages/home.js
@@ -1,27 +1,21 @@
 import React from "react"
 import PageHeader from "../components/pageHeader";
 
-class HomePage extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+function HomePage() {
+  const style = {
+    background: "white",
+  };
 
-  render() {
-    const style = {
-      background: "white",
-    };
-  
-    let content = (
-      <>
-        <PageHeader />
-        <div style={style}>
-          <a href="/vehicles">Vehicle List</a>
-        </div>
-      </>
-    );
-  
-    return content;
-  }
+  let content = (
+    <>
+      <PageHeader />
+      <div style={style}>
+        <a href="/vehicles">Vehicle List</a>
+      </div>
+    </>
+  );
+
+  return content;
 }
 
 export default HomePage;
--- ui/src/pages/vehicles.js
+++ ui/src/pages/vehicles.js
@@ -2,27 +2,21 @@ import React from "react"
 import VehicleCardList from "../components/vehicleCardList";
 import PageHeader from "../components/pageHeader";
 
-class VehiclesPage extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+function VehiclesPage() {
+  const style = {
+    background: "white",
+  };
 
-  render() {
-    const style = {
-      background: "white",
-    };
-  
-    let content = (
-      <>
-        <PageHeader />
-        <div style={style}>
-          <VehicleCardList />
-        </div>
-      </>
-    );
-  
-    return content;
-  }
+  let content = (
+    <>
+      <PageHeader />
+      <div style={style}>
+        <VehicleCardList />
+      </div>
+    </>
+  );
+
+  return content;
 }
 
 export default VehiclesPage;
