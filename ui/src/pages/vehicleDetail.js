e288ad8b223b8f42430b5b9bb32c001931889127
Jamie Sweetland <slimindie@gmail.com>
2023-10-19 11:07:48 -0400

    fix: correctly retrieve the vin from the detail page route

--- ui/src/pages/vehicleDetail.js
+++ ui/src/pages/vehicleDetail.js
@@ -1,32 +1,26 @@
 import React from "react"
 import PageHeader from "../components/pageHeader";
+import {useParams} from "react-router-dom";
 
-class VehicleDetailPage extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+function VehicleDetailPage() {
+  const style = {
+    background: "white",
+  };
 
-  render() {
-    console.log(JSON.stringify(this.props))
-
-    const style = {
-      background: "white",
-    };
-
-    console.log("this.props = ", JSON.stringify(this.props))
-    // console.log("VIN=", this.props.match.params.vin)
+  const {vin} = useParams()
+  console.log("vin = ", JSON.stringify(vin))
+  // console.log("VIN=", this.props.match.params.vin)
   
-    let content = (
-      <>
-        <PageHeader />
-        <div style={style}>
-          {/* <div>Detailed Information for VIN {this.props.match.params.vin}</div> */}
-        </div>
-      </>
-    );
-  
-    return content;
-  }
+  let content = (
+    <>
+      <PageHeader />
+      <div style={style}>
+        <div>Detailed Information for VIN {vin}</div>
+      </div>
+    </>
+  );
+
+  return content;
 }
 
 export default VehicleDetailPage;
