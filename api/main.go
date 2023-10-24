45891eef28e6e0c88cedb88503fff271b62d6046
Jamie Sweetland <slimindie@gmail.com>
2023-10-24 13:42:34 -0400

    feat: clean up logging and query parameter parsing

--- api/main.go
+++ api/main.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"net/url"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -113,14 +112,9 @@ func methodNotAllowedResponse(w *http.ResponseWriter, h string) {
 
 // handles any unhandled routes
 func getBaseRoute(w http.ResponseWriter, r *http.Request) {
-	h := "getBaseRoute"
-
-	s := "no functionality is implemented at this endpoint"
 	m := SimpleMessage{
-		Message: s,
+		Message: "no functionality is implemented at this endpoint",
 	}
-
-	fmt.Printf("  [%s] %s\n", h, s)
 
 	enableCors(&w)
 	setJSONContentType(&w)
@@ -130,102 +124,83 @@ func getBaseRoute(w http.ResponseWriter, r *http.Request) {
 // GET /about
 // returns the about info for the application
 func getAbout(w http.ResponseWriter, r *http.Request) {
-	h := "getAbout"
-
-	switch r.Method {
-	case http.MethodGet:
-		enableCors(&w)
-		setJSONContentType(&w)
-		json.NewEncoder(w).Encode(AboutInfo)
-	default:
-		methodNotAllowedResponse(&w, h)
-	}
+	enableCors(&w)
+	setJSONContentType(&w)
+	json.NewEncoder(w).Encode(AboutInfo)
 }
 
 // GET /vehicles
 // returns the vehicles stored in the database
 func getVehicles(w http.ResponseWriter, r *http.Request) {
-	h := "getVehicles"
-	logHandler(h, r.Method)
+	var id int
+	var vin string
+	var make string
+	var model string
+	var year int
+	var trim string
+	var trimpackage string
+	var nickname string
+	var colorname string
+	var colorhex string
+	var vehicles = []Vehicle{}
 
-	switch r.Method {
-	case http.MethodGet:
-		var id int
-		var vin string
-		var make string
-		var model string
-		var year int
-		var trim string
-		var trimpackage string
-		var nickname string
-		var colorname string
-		var colorhex string
-		var vehicles = []Vehicle{}
-
-		// connect to database
-		db, err := db.Connect()
-		if err != nil {
-			errorResponse(&w, "An error occurred while connecting to the database.", err)
-		}
-
-		// query the database for vehicles
-		q := "SELECT id, vin, make, model, year, trim, package, nickname, colorname, colorhex FROM vehicles"
-		rows, err := db.Query(q)
-		defer rows.Close()
-		if err != nil {
-			errorResponse(&w, "An error occurred while querying the vehicles table in the database.", err)
-		}
-
-		// load the rows of data into an array
-		for rows.Next() {
-			rows.Scan(
-				&id,
-				&vin,
-				&make,
-				&model,
-				&year,
-				&trim,
-				&trimpackage,
-				&nickname,
-				&colorname,
-				&colorhex,
-			)
-			v := Vehicle{
-				ID:       id,
-				VIN:      vin,
-				Make:     make,
-				Model:    model,
-				Year:     year,
-				Trim:     trim,
-				Package:  trimpackage,
-				Nickname: nickname,
-				Color: Color{
-					Name: colorname,
-					Hex:  colorhex,
-				},
-			}
-			vehicles = append(vehicles, v)
-		}
-
-		// send the data back in the response
-		enableCors(&w)
-		setJSONContentType(&w)
-		json.NewEncoder(w).Encode(vehicles)
-	default:
-		methodNotAllowedResponse(&w, h)
+	// connect to database
+	db, err := db.Connect()
+	if err != nil {
+		errorResponse(&w, "An error occurred while connecting to the database.", err)
 	}
+
+	// query the database for vehicles
+	q := "SELECT id, vin, make, model, year, trim, package, nickname, colorname, colorhex FROM vehicles"
+	rows, err := db.Query(q)
+	if err != nil {
+		errorResponse(&w, "An error occurred while querying the vehicles table in the database.", err)
+	}
+	defer rows.Close()
+
+	// load the rows of data into an array
+	for rows.Next() {
+		rows.Scan(
+			&id,
+			&vin,
+			&make,
+			&model,
+			&year,
+			&trim,
+			&trimpackage,
+			&nickname,
+			&colorname,
+			&colorhex,
+		)
+		v := Vehicle{
+			ID:       id,
+			VIN:      vin,
+			Make:     make,
+			Model:    model,
+			Year:     year,
+			Trim:     trim,
+			Package:  trimpackage,
+			Nickname: nickname,
+			Color: Color{
+				Name: colorname,
+				Hex:  colorhex,
+			},
+		}
+		vehicles = append(vehicles, v)
+	}
+
+	// send the data back in the response
+	enableCors(&w)
+	setJSONContentType(&w)
+	json.NewEncoder(w).Encode(vehicles)
 }
 
 // GET /vinlookup?vin={vin}
 // look up the data for the specified vin
 func getVINLookup(w http.ResponseWriter, r *http.Request) {
-	h := "getVinLookup"
-	logHandler(h, r.Method)
-
 	// get the vin value from the parameter
 	v := r.URL.Query().Get("vin")
 	if v == "" {
-
 		errorResponse(&w, "No VIN value was provided", nil)
 	} else {
 		// look up the vin data
@@ -244,29 +219,47 @@ func getVINLookup(w http.ResponseWriter, r *http.Request) {
 // POST /storevindata
 // store the specified vin data in the database
 func postStoreVINData(w http.ResponseWriter, r *http.Request) {
-	h := "postStoreVINData"
-	logHandler(h, r.Method)
-
-	switch r.Method {
-	case http.MethodPost:
-		// parse the complete url from the request
-		u, err := url.Parse(r.URL.String())
+	// parse the query parameters from the complete request url
+	v := r.URL.Query().Get("vin")
+	if v == "" {
+		// decode the request body
+		var d vin.LookupResult
+		err := json.NewDecoder(r.Body).Decode(&d)
 		if err != nil {
-			errorResponse(&w, "An error occurred while parsing the URL for VIN lookup", err)
-		}
+			errorResponse(&w, "An error occurred while decoding the request body, the body may not have been provided", err)
+		} else {
+			// store the provided vin data
 
-		// parse the query parameters from the complete request url
-		q, _ := url.ParseQuery(u.RawQuery)
-		if len(q) == 0 || q["vin"] == nil {
-			fmt.Printf("  [%s] no vin provided, will use vin data in request body\n", h)
-
-			// decode the request body
-			var d vin.LookupResult
-			err = json.NewDecoder(r.Body).Decode(&d)
+			// connect to database
+			dbconn, err := db.Connect()
 			if err != nil {
-				errorResponse(&w, "An error occurred while decoding the request body, the body may not have been provided", err)
+				errorResponse(&w, "An error occurred while connecting to the database.", err)
+			}
+
+			// insert the data into the vindata table
+			v, err := db.InsertVINData(dbconn, d)
+			if err != nil {
+				errorResponse(&w, "An error occurred while attempting to insert the data into the database", err)
 			} else {
-				// store the provided vin data
+				// send back a success message
+				messageResponse(&w, fmt.Sprintf("Data for VIN %s was successfully inserted", v))
+			}
+		}
+	} else {
+		// look up the vin data
+		d, err := vin.Lookup(v)
+		if err != nil {
+			errorResponse(&w, "An error occured while looking up the VIN data", err)
+		} else {
+			// make sure there's one and only one record for the specified vin
+			if d.Count == 0 {
+				// there are no records
+				errorResponse(&w, "No data results were found for the specified VIN", nil)
+			} else if d.Count > 1 {
+				// there are two or more records
+				errorResponse(&w, fmt.Sprintf("%d results were found for the specified VIN", d.Count), nil)
+			} else {
+				// there is a single record, store the retrieved data
 
 				// connect to database
 				dbconn, err := db.Connect()
@@ -275,70 +268,17 @@ func postStoreVINData(w http.ResponseWriter, r *http.Request) {
 				}
 
 				// insert the data into the vindata table
-				v, err := db.InsertVINData(dbconn, d)
+				v, err := db.InsertVINData(dbconn, d.Results[0])
 				if err != nil {
 					errorResponse(&w, "An error occurred while attempting to insert the data into the database", err)
 				} else {
 					// send back a success message
-					fmt.Printf("  [%s] data for VIN %s successfully inserted", h, v)
 					messageResponse(&w, fmt.Sprintf("Data for VIN %s was successfully inserted", v))
 				}
 			}
-		} else {
-			// extract the vin from the query parameters
-			v := q["vin"][0]
-			fmt.Printf("  [%s] vin %s provided, will look up data to store\n", h, v)
-
-			// look up the vin data
-			d, err := vin.Lookup(v)
-			if err != nil {
-				errorResponse(&w, "An error occured while looking up the VIN data", err)
-			} else {
-				// make sure there's one and only one record for the specified vin
-				if d.Count == 0 {
-					// there are no records
-					errorResponse(&w, "No data results were found for the specified VIN", nil)
-				} else if d.Count > 1 {
-					// there are two or more records
-					errorResponse(&w, fmt.Sprintf("%d results were found for the specified VIN", d.Count), nil)
-				} else {
-					// there is a single record, store the retrieved data
-					fmt.Printf("  [%s] data for VIN %s successfully retrieved", h, v)
-
-					// connect to database
-					dbconn, err := db.Connect()
-					if err != nil {
-						errorResponse(&w, "An error occurred while connecting to the database.", err)
-					}
-
-					// insert the data into the vindata table
-					v, err := db.InsertVINData(dbconn, d.Results[0])
-					if err != nil {
-						errorResponse(&w, "An error occurred while attempting to insert the data into the database", err)
-					} else {
-						// send back a success message
-						fmt.Printf("  [%s] data for VIN %s successfully inserted", h, v)
-						messageResponse(&w, fmt.Sprintf("Data for VIN %s was successfully inserted", v))
-					}
-				}
-			}
 		}
-	default:
-		methodNotAllowedResponse(&w, h)
 	}
 }
-
-// // handle the request routes
-// func handleRequests() {
-// 	http.HandleFunc("/", getBaseRoute)
-// 	http.HandleFunc("/about", getAbout)
-// 	http.HandleFunc("/vehicles", getVehicles)
-// 	http.HandleFunc("/vinlookup", getVINLookup)
-// 	http.HandleFunc("/storevindata", postStoreVINData)
-
-// 	fmt.Printf("listening on port %s\n", port)
-// 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
-// }
 
 func startServer() {
 	r := chi.NewRouter()
